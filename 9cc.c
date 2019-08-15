654051f52ce6780a09d47412515c9435118cdb73
Terry Huang <u0551076@nkust.edu.tw>
2019-08-15 13:45:03 +0800

    Improve error message

--- 9cc.c
+++ 9cc.c
@@ -23,12 +23,26 @@ struct Token {
 
 // Current processsing token
 Token *token;
-
+char *user_input;
 // Process error function
 // like printf argument
 void error( char* fmt, ... ) {
 	va_list ap;
 	va_start( ap, fmt );
+	vfprintf( stderr, fmt, ap );
+	fprintf( stderr, "\n" );
+	exit(1);
+}
+
+// report error position
+void error_at( char *loc, char *fmt, ... ) {
+	va_list ap;
+	va_start( ap, fmt );
+
+	int pos = loc - user_input;
+	fprintf( stderr, "%s\n", user_input );
+	fprintf( stderr, "%*s", pos, "" );
+	fprintf( stderr, "^ " );
 	vfprintf( stderr, fmt, ap );
 	fprintf( stderr, "\n" );
 	exit(1);
@@ -54,7 +68,7 @@ void expect( char op ) {
 // number; or show error 
 int expect_number() {
 	if( token->kind != TK_NUM )
-		error("Not number");
+		error_at( token->str ,"Not number");
 	int val = token->val;
 	token = token->next;
 	return val;
@@ -110,6 +124,7 @@ int main( int argc, char **argv) {
         return 1;
 	}
 	
+	user_input = argv[1];
 	token = tokenize(argv[1]);
     
     printf(".intel_syntax noprefix\n");
